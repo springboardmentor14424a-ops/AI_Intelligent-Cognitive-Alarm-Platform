2b2c25dfd6fff1a0736896f0e07a8f2dfc0a3034
Rachana D N <rachanadn27@gmail.com>
2026-09-11 22:25:11 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,11 @@
-from api.app import app
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+if CURRENT_DIR not in sys.path:
+    sys.path.insert(0, CURRENT_DIR)
+
+from app import app
 from mangum import Mangum
 
 handler = Mangum(app, lifespan="off")
